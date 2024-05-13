1ad3e69eb9c81709a53fa5c65f2f032fcc10798d
Sanzhar Nussipbek <Nsanzhar.99@gmail.com>
2024-05-13 23:16:51 +0800

    fix: movies carousel

--- src/components/movies-carousel.tsx
+++ src/components/movies-carousel.tsx
@@ -12,6 +12,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getMovieDetailsById } from '@/services/tmdb/movie';
 import { useEffect } from 'react';
 import { Skeleton } from './ui/skeleton';
+import { MovieDetails } from '@/types/tmdb/movie';
 
 export default function MoviesCarousel({
   data,
@@ -20,21 +21,18 @@ export default function MoviesCarousel({
 }) {
   const {
     data: movieDetails,
-    isLoading,
+    isFetching,
     refetch,
   } = useQuery({
-    queryKey: [
-      'latest-movies-details',
-      ...(data?.result.items || []).map((movie) => movie.tmdb_id),
-    ],
-    queryFn: () => {
+    queryKey: ['latest-movies-details', JSON.stringify(data)],
+    queryFn: async () => {
       if (!data) return [];
-      return Promise.all(
+      const res = await Promise.all(
         data.result.items.map((movie) => getMovieDetailsById(movie.tmdb_id))
       );
+      return res.filter((movie) => movie !== null);
     },
     enabled: !!data,
-    select: (data) => data.filter((movie) => movie !== null),
     initialData: [],
     placeholderData: [],
   });
@@ -56,33 +54,35 @@ export default function MoviesCarousel({
       className='w-full max-w-5xl'
     >
       <CarouselContent>
-        {isLoading &&
+        {isFetching &&
           Array.from({ length: 8 }).map((_, index) => (
             <CarouselItem key={index} className='basis-1/8'>
               <Skeleton className='w-24 aspect-[2/3] rounded' />
             </CarouselItem>
           ))}
-        {movieDetails.map((movie) => (
-          <CarouselItem key={movie.id} className='basis-1/8'>
-            <Link
-              key={movie.id}
-              href={`/movies/${movie.id}`}
-              className='flex flex-col items-center gap-2 relative hover:opacity-75 hover:scale-[1.025] transition-all duration-300 ease-in-out'
-            >
-              <img
-                className='w-24 aspect-[2/3] rounded'
-                src={`${process.env.NEXT_PUBLIC_TMDB_IMAGE_URL}${movie.poster_path}`}
-                alt={movie.title}
-              />
-              <div className='flex flex-col text-center absolute bottom-0 backdrop-blur-[1px] w-full p-0.5 bg-gradient-to-b from-transparent to-slate-900'>
-                <h2 className='text-xs font-bold'>{movie.title}</h2>
-                <p className='text-xs'>
-                  {new Date(movie.release_date).getFullYear()}
-                </p>
-              </div>
-            </Link>
-          </CarouselItem>
-        ))}
+        {movieDetails.map((movie) =>
+          movie ? (
+            <CarouselItem key={movie.id} className='basis-1/8'>
+              <Link
+                key={movie.id}
+                href={`/movies/${movie.id}`}
+                className='flex flex-col items-center gap-2 relative hover:opacity-75 hover:scale-[1.025] transition-all duration-300 ease-in-out'
+              >
+                <img
+                  className='w-24 aspect-[2/3] rounded'
+                  src={`${process.env.NEXT_PUBLIC_TMDB_IMAGE_URL}${movie.poster_path}`}
+                  alt={movie.title}
+                />
+                <div className='flex flex-col text-center absolute bottom-0 backdrop-blur-[1px] w-full p-0.5 bg-gradient-to-b from-transparent to-slate-900'>
+                  <h2 className='text-xs font-bold'>{movie.title}</h2>
+                  <p className='text-xs'>
+                    {new Date(movie.release_date).getFullYear()}
+                  </p>
+                </div>
+              </Link>
+            </CarouselItem>
+          ) : null
+        )}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
