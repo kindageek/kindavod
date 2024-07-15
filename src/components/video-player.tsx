192672795ef0f933a8bd080a8c6179571b309022
Sanzhar Nussipbek <Nsanzhar.99@gmail.com>
2024-07-15 20:41:34 +0800

    feat: tv search params

--- src/components/video-player.tsx
+++ src/components/video-player.tsx
@@ -2,6 +2,8 @@
 
 import Link from 'next/link';
 import { Info } from 'lucide-react';
+import { useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
 
 export default function VideoPlayer({
   id,
@@ -10,11 +12,26 @@ export default function VideoPlayer({
   id: string;
   type: 'movie' | 'tv';
 }) {
+  const searchParams = useSearchParams();
+  const season = searchParams.get('s');
+  const episode = searchParams.get('ep');
+
+  const videoUrl = useMemo(() => {
+    const defaultUrl = `${process.env.NEXT_PUBLIC_VIDSRC_URL}/embed/${type}/${id}`;
+    if (type === 'movie') {
+      return defaultUrl;
+    }
+    if (season && episode) {
+      return `${defaultUrl}/${season}/${episode}`;
+    }
+    return defaultUrl;
+  }, [id, type, season, episode]);
+
   return (
     <section className='flex flex-col items-center w-full gap-4'>
       <iframe
         allowFullScreen
-        src={`${process.env.NEXT_PUBLIC_VIDSRC_URL}/embed/${type}/${id}`}
+        src={videoUrl}
         className='w-full aspect-video object-cover rounded'
       />
       <div className='flex items-center justify-center gap-1 text-center text-sm text-gray-500 hover:text-white w-full'>
