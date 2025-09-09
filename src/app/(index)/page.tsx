5c62e2051fab8c425a20b460a75c816a22557665
Sanzhar Nussipbek <Nsanzhar.99@gmail.com>
2025-09-09 21:54:38 +0800

    perf: optimization

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -12,8 +12,9 @@ import {
   getPopularMovies,
   getTrendingList,
   getTvShowsByCompanyId,
-  ITmdbListResponse,
 } from '@/services/tmdb';
+
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
   const [
